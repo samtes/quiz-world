139dd836fe4e5670f144ef941af6b4c7e61bc989
samtes <sweldemariam@gmail.com>
2015-12-16 23:46:10 -0800

    test(user): Adds acceptance test for /users endpoint

---
+++ test/acceptance/users_spec.js
@@ -0,0 +1,267 @@
+process.env.DBNAME = "quiz-world-test";
+var expect = require("chai").expect;
+var request = require("supertest");
+var fs = require("fs");
+var exec = require("child_process").exec;
+var app = require("../../lib/app");
+var expect = require("chai").expect;
+var User, id, initMongo, token, cookie;
+
+describe("users route", function(){
+  before(function (done) {
+    User = require("../../lib/models/user");
+    initMongo = require("../../lib/server/init-mongo");
+    done();
+  });
+
+  beforeEach(function (done) {
+    initMongo.db.collection("users").drop();
+
+    new User({
+      email: "foo@test.com",
+      password: "Password1"
+    }).register(function (err, user) {
+      expect(user._id.toString()).to.have.length(24);
+
+      new User({
+        email: "zoo@test.com",
+        password: "Password1"
+      }).register(function (err, user) {
+        expect(user._id.toString()).to.have.length(24);
+        done();
+      });
+    });
+  });
+
+  describe("GET /users", function () {
+    it("should return all users", function (done) {
+      request(app)
+      .post("/login")
+      .send({
+        "email": "foo@test.com",
+        "password": "Password1",
+        "session": "session"
+      })
+      .end(function (err, res) {
+        token = res.body.token;
+        cookie = res.headers['set-cookie'];
+        expect(res.status).to.equal(200);
+
+        request(app)
+        .get('/users')
+        .set('cookie', cookie)
+        .set("session-id", token)
+        .expect(200, done);
+      });
+    });
+  });
+
+  describe("GET /users/:id", function () {
+    it("should return all users", function (done) {
+      request(app)
+      .post("/login")
+      .send({
+        "email": "foo@test.com",
+        "password": "Password1",
+        "session": "session"
+      })
+      .end(function (err, res) {
+        id = res.body.userID;
+        token = res.body.token;
+        cookie = res.headers['set-cookie'];
+        expect(res.status).to.equal(200);
+
+        request(app)
+        .get('/users/'.concat(id))
+        .set('cookie', cookie)
+        .set("session-id", token)
+        .expect(200, done);
+      });
+    });
+
+    it("should return 404", function (done) {
+      request(app)
+      .post("/login")
+      .send({
+        "email": "foo@test.com",
+        "password": "Password1",
+        "session": "session"
+      })
+      .end(function (err, res) {
+        id = res.body.userID;
+        token = res.body.token;
+        cookie = res.headers['set-cookie'];
+        expect(res.status).to.equal(200);
+
+        request(app)
+        .get('/users/'.concat("123412341234"))
+        .set('cookie', cookie)
+        .set("session-id", token)
+        .expect(404, done);
+      });
+    });
+  });
+
+  describe("PUT /users/:id", function () {
+    it("should update users", function (done) {
+      request(app)
+      .post("/login")
+      .send({
+        "email": "foo@test.com",
+        "password": "Password1",
+        "session": "session"
+      })
+      .end(function (err, res) {
+        id = res.body.userID;
+        token = res.body.token;
+        cookie = res.headers['set-cookie'];
+        expect(res.status).to.equal(200);
+
+        request(app)
+        .put('/users/'.concat(id))
+        .set('cookie', cookie)
+        .set("session-id", token)
+        .send({"email": "foof@test.com"})
+        .end(function (err, res) {
+          expect(res.status).to.equal(201);
+          expect(res.body.message).to.equal("User successfully updated.");
+          done();
+        });
+      });
+    });
+
+    it("should return email taken", function (done) {
+      request(app)
+      .post("/login")
+      .send({
+        "email": "foo@test.com",
+        "password": "Password1",
+        "session": "session"
+      })
+      .end(function (err, res) {
+        id = res.body.userID;
+        token = res.body.token;
+        cookie = res.headers['set-cookie'];
+        expect(res.status).to.equal(200);
+
+        request(app)
+        .put('/users/'.concat(id))
+        .set('cookie', cookie)
+        .set("session-id", token)
+        .send({"email": "zoo@test.com"})
+        .end(function (err, res) {
+          expect(res.status).to.equal(422);
+          expect(res.body.message).to.equal("Email already taken.");
+          done();
+        });
+      });
+    });
+
+    it("should return invalid email missing \"@\"", function (done) {
+      request(app)
+      .post("/login")
+      .send({
+        "email": "foo@test.com",
+        "password": "Password1",
+        "session": "session"
+      })
+      .end(function (err, res) {
+        id = res.body.userID;
+        token = res.body.token;
+        cookie = res.headers['set-cookie'];
+        expect(res.status).to.equal(200);
+
+        request(app)
+        .put('/users/'.concat(id))
+        .set('cookie', cookie)
+        .set("session-id", token)
+        .send({"email": "zootest.com"})
+        .end(function (err, res) {
+          expect(res.status).to.equal(400);
+          expect(res.body.message).to.equal("Invalid email.");
+          done();
+        });
+      });
+    });
+
+    it("should return invalid email missing \".\"", function (done) {
+      request(app)
+      .post("/login")
+      .send({
+        "email": "foo@test.com",
+        "password": "Password1",
+        "session": "session"
+      })
+      .end(function (err, res) {
+        id = res.body.userID;
+        token = res.body.token;
+        cookie = res.headers['set-cookie'];
+        expect(res.status).to.equal(200);
+
+        request(app)
+        .put('/users/'.concat(id))
+        .set('cookie', cookie)
+        .set("session-id", token)
+        .send({"email": "zoo@testcom"})
+        .end(function (err, res) {
+          expect(res.status).to.equal(400);
+          expect(res.body.message).to.equal("Invalid email.");
+          done();
+        });
+      });
+    });
+
+    it("should return invalid password", function (done) {
+      request(app)
+      .post("/login")
+      .send({
+        "email": "foo@test.com",
+        "password": "Password1",
+        "session": "session"
+      })
+      .end(function (err, res) {
+        id = res.body.userID;
+        token = res.body.token;
+        cookie = res.headers['set-cookie'];
+        expect(res.status).to.equal(200);
+
+        request(app)
+        .put('/users/'.concat(id))
+        .set('cookie', cookie)
+        .set("session-id", token)
+        .send({"password": "password1"})
+        .end(function (err, res) {
+          expect(res.status).to.equal(400);
+          expect(res.body.message).to.equal("Invalid password.");
+          done();
+        });
+      });
+    });
+
+    it("should return invalid email", function (done) {
+      request(app)
+      .post("/login")
+      .send({
+        "email": "foo@test.com",
+        "password": "Password1",
+        "session": "session"
+      })
+      .end(function (err, res) {
+        id = res.body.userID;
+        token = res.body.token;
+        cookie = res.headers['set-cookie'];
+        expect(res.status).to.equal(200);
+
+        request(app)
+        .put('/users/'.concat(id))
+        .set('cookie', cookie)
+        .set("session-id", token)
+        .end(function (err, res) {
+          expect(res.status).to.equal(400);
+          expect(res.body.message).to.equal("Invalid request.");
+          done();
+        });
+      });
+    });
+  });
+});
