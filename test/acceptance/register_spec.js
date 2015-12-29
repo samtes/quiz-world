7c1bb70fb324c4e0c64e990ea3a9d17f376f2b2d
samtes <sweldemariam@gmail.com>
2015-12-29 11:33:05 -0800

    feat(session): Adds session check to reqister route and tests

--- test/acceptance/register_spec.js
+++ test/acceptance/register_spec.js
@@ -5,12 +5,13 @@ var fs = require("fs");
 var exec = require("child_process").exec;
 var app = require("../../lib/app");
 var expect = require("chai").expect;
-var User, initMongo;
-var cookie;
+var User, Session, initMongo;
+var cookie, sessionId;
 
 describe("Register route", function(){
   before(function (done) {
     User = require("../../lib/models/user");
+    Session = require("../../lib/models/session");
     initMongo = require("../../lib/server/init-mongo");
     done();
   });
@@ -23,14 +24,25 @@ describe("Register route", function(){
       password: "Password1"
     }).register(function (err, user) {
       expect(user._id.toString()).to.have.length(24);
-      done();
+
+      new Session({
+        quantity: 8,
+        difficulty: 1,
+        type: ["css","html"],
+        questions: ["123412341230", "123412341231", "123412341232", "123412341233", "123412341234", "123412341235", "123412341236", "123412341237"]
+      }).insert(function (err, session) {
+        sessionId = session._id.toString();
+
+        expect(session._id.toString()).to.have.length(24);
+        done();
+      })
     });
   });
 
   describe("POST /register", function () {
     it("should register user", function (done) {
       request(app)
-      .post("/register")
+      .post("/register?session-id=".concat(sessionId))
       .send({
         "email": "bobby@test.com",
         "password": "Password1",
@@ -38,7 +50,13 @@ describe("Register route", function(){
       })
       .end(function (err, res) {
         expect(res.status).to.equal(201);
-        done();
+
+        Session.findById(sessionId, function (err, session) {
+          User.findByEmail("bobby@test.com", function (err, user) {
+            expect(session.userId).to.be.eql(user._id.toString());
+            done();
+          });
+        });
       });
     });
 
@@ -94,9 +112,25 @@ describe("Register route", function(){
       });
     });
 
-    it("should not register duplicate user", function (done) {
+    it("should not register without a session", function (done) {
       request(app)
       .post("/register")
+      .send({
+        "email": "samiboy@test.com",
+        "password": "Password1",
+        "confirmPassword": "Password1"
+      })
+      .end(function (err, res) {
+        expect(res.status).to.equal(404);
+        expect(res.body.message).to.equal("Session not found.");
+        done();
+      });
+    });
+
+
+    it("should not register duplicate user", function (done) {
+      request(app)
+      .post("/register?session-id=".concat(sessionId))
       .send({
         "email": "sami@test.com",
         "password": "Password1",
